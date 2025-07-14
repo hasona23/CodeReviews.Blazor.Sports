20cef9f4b5d95b7ced18dc84b2ec47826ed9e768
hasona23 <130175104+hasona23@users.noreply.github.com>
2025-07-14 22:50:04 +0300

    View matches page

---
+++ SportStatistics.Hasona23/SportStatisticsApp/PalleteColors.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using MudBlazor.Utilities;
+
+namespace SportStatisticsApp;
+
+public static class PalleteColors
+{
+    public static readonly MudColor Primary = new (255,101,0,255);
+    public static readonly MudColor Secondary = new (30,62,98,255);
+    public static readonly MudColor LightBlack = new (11,25,44,255);
+    public static readonly MudColor Black = new (0,0,0,255);
+}
